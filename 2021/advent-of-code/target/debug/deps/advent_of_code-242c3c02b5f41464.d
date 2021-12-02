193d85c702ef6fc75d5477183b6bfabf77be0a9b
Elias Roussos <eliasisaiah@outlook.com>
2021-12-01 19:14:06 -0500

    added .gitignore

--- 2021/advent-of-code/target/debug/deps/advent_of_code-242c3c02b5f41464.d
+++ 2021/advent-of-code/target/debug/deps/advent_of_code-242c3c02b5f41464.d
@@ -1,5 +1,5 @@
-/home/elias/repositories-linux/advent-of-code/2021/advent-of-code/target/debug/deps/advent_of_code-242c3c02b5f41464: src/main.rs
+/mnt/c/Users/elias/repositories/advent-of-code/2021/advent-of-code/target/debug/deps/advent_of_code-242c3c02b5f41464: src/main.rs
 
-/home/elias/repositories-linux/advent-of-code/2021/advent-of-code/target/debug/deps/advent_of_code-242c3c02b5f41464.d: src/main.rs
+/mnt/c/Users/elias/repositories/advent-of-code/2021/advent-of-code/target/debug/deps/advent_of_code-242c3c02b5f41464.d: src/main.rs
 
 src/main.rs:
