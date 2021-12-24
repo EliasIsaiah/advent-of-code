aceeae69d2c3b12a2e6c79d63456db8c45eb5e36
Elias Roussos <eliasisaiah@outlook.com>
2021-12-24 02:24:35 -0500

    first function created

--- 2021/2/two/src/lib.rs
+++ 2021/2/two/src/lib.rs
@@ -1,8 +1,18 @@
+pub fn get_foreward_total(foreward_values: Vec<&str>) -> i64 {
+    3
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn forward_three() {
+        let input = vec!["foreward 3"];
+        let expected: i64 = 3;
+        let actual = get_foreward_total(input);
+        assert_eq!(expected, actual);
     }
+    fn down_three() {}
+
+    fn forward_three_down_three() {}
 }
